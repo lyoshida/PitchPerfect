e48f63ca830fcd266d6f942918ee53d749c4479c
Luis Yoshida <luis.yoshida@gmail.com>
2015-05-09 15:01:16 -0300

    End of Lesson 3a. Created the view for the first screen and a second UIViewController.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // weak and strong keywords has to do with memory management
+    // the ! means optional value.
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide the stop button
+        stopButton.hidden = true
+        recordButton.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // The IBAction (Interface Builder Action) tells that this function is linked to the storyboard.
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Show text "recording in progress"
+        //TODO: Record the user's voice
 
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        recordingInProgress.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
 }
 
